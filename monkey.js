67cee6938c5c7c3b6c1b33ca2a1a3f217b837854
Nirmesh <nirmesh.khandelwal@gmail.com>
2015-12-01 08:01:52 -0500

    updating monkey

--- monkey.js
+++ monkey.js
@@ -1,30 +1,11 @@
 var request = require('request')
+var io = require('socket.io')(5001);
 
 
  
 //n = 2   // Number of burst requests that we sent out in an interval
 
 var app_url = 'http://162.243.55.160:8080/mathy';
-
-/*
-var refreshId = setInterval( function () {   
-    // A key for this burst of intervals
-    key = n //Math.floor(Date.now() / 1000)
-    var startTime = Date.now();
-    allLatency[key] = 0
-
-    if(n > 65536){
-        console.log(JSON.stringify(allLatency));
-        clearInterval(refreshId)
-    }
-
-    
-
-    n += 100   // Double the burst size
-}, 2000);
-
-*/
-
 var n = 0
 var allLatency = {} 
 var allErrors = {}
@@ -36,18 +17,14 @@ var loop = function(){
     allLatency[key] = 0
     allErrors[key] = 0
     c = 0
-    
-    
+       
     for(var i=0; i<=n; i++){    
         request({url: app_url}, function (error, res, body) 
         {
             latency = Date.now() - startTime;
             if(error){
                 allErrors[key] += 1;
-                console.log("error in request to home site - Latency" );
-                //console.log(error)
-                //console.log('#######################################\n\n\n')
-                
+                console.log("error in request to home site - Latency" );                
             }
             c++
             allLatency[key] += latency/n
@@ -58,12 +35,14 @@ var loop = function(){
 
 loop();
 
-
-
-setInterval(function(){
+io.on('connection', function(socket){
+  setInterval(function(){
+    io.emit('error', JSON.stringify(allErrors));
+    io.emit('latency', JSON.stringify(allLatency));
     console.log('###################')
     console.log(allLatency);
     console.log('###################')
     console.log(allErrors);
-    console.log('\n\n\n')
-}, 4000);
+    console.log('\n\n\n');
+  }, 4000);
+});
